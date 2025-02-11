c4257bcfc72badf0b5c82462ba72c1d70ba0c0ed
David Menezes <daavidmenezzess@gmail.com>
2025-02-11 13:29:00 -0300

    Criacao do service estoque

--- eculturese/src/main/java/com/project/services/EstoqueService.java
+++ eculturese/src/main/java/com/project/services/EstoqueService.java
@@ -1,16 +1,24 @@
 package com.project.services;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import com.project.models.Estoque;
 import com.project.models.Produto;
+import com.project.repositores.Impl.ProdutoImpl;
 
 public class EstoqueService {
 
     // REGRA DE NEGOCIO BASICA, QUANDO VAI ADICIONAR UM PRODUTO A QUANTIDADE TEM QUER SER
     // QNT > 0
+    MongoClient mongoClient = new MongoClient("localhost", 27017);
+    MongoDatabase database = mongoClient.getDatabase("teste");
+    ProdutoImpl produtoRepository = new ProdutoImpl(database);
 
     public void Adicionar(Estoque estoque, Produto produto) {
         if (produto.getQuantidade() > 0) {
             estoque.adicionarProduto(produto);
+            produtoRepository.registrar(produto);
+
         }else{
             System.out.println("Produto invalido, adicione quantidade");
             return;
--- eculturese/src/main/java/com/project/testesUnitarios/testesService/TesteService.java
+++ eculturese/src/main/java/com/project/testesUnitarios/testesService/TesteService.java
@@ -1,4 +1,27 @@
 package com.project.testesUnitarios.testesService;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.project.models.Estoque;
+import com.project.models.Produto;
+import com.project.repositores.Impl.ProdutoImpl;
+import com.project.services.EstoqueService;
+
 public class TesteService {
+    public static void main(String[] args) {
+        //ProdutoImpl produtoRepository = new ProdutoImpl(database);
+        Estoque estoque = new Estoque();
+        Produto produto = new Produto("tf","Elettronicos", 142.0,99, "4323232");
+        EstoqueService Eservice = new EstoqueService();
+        Eservice.Adicionar(estoque,produto);
+
+        //Mostra todos
+        //String mostra = produtoRepository.BuscarTodos().toString();
+        //System.out.println(mostra);
+
+        //Atualiza quant do produto
+        //produtoRepository.atualizarProduto(produto);
+
+        //produtoRepository.excluirProduto(produto);
+    }
 }
